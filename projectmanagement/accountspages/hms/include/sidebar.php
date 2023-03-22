24a2825eb04d1717f16948a62015b6d027cff3d6
Tabassum <66271841+tabassumraza@users.noreply.github.com>
2023-03-22 16:09:58 +0500

    Add files via upload

---
+++ projectmanagement/accountspages/hms/include/sidebar.php
@@ -0,0 +1,57 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Dashboard </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="submitmyproject.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-pencil-alt"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> My Project </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li>
+								<a href="projectdetail.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Project Details</span>
+										</div>
+									</div>
+								</a>
+							</li>
+<li>
+								
+							</li>
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
